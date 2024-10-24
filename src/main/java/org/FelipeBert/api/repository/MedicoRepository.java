11b3a9148e3f085e19ae31494957a1d5bbe87a83
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-24 14:15:19 -0300

    feat: add MedicoRepository with pagination for active doctors
    - Created `MedicoRepository` interface extending `JpaRepository` for the `Medico` entity.
    - Implemented method `findAllByAtivoTrue(Pageable paginacao)` to retrieve paginated results of only active doctors.

---
+++ src/main/java/org/FelipeBert/api/repository/MedicoRepository.java
@@ -0,0 +1,10 @@
+package org.FelipeBert.api.repository;
+
+import org.FelipeBert.api.model.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+}
