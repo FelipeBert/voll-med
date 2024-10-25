12a580c596ba4e5b09e5ae03b34e10dea3f9b716
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-25 20:10:58 -0300

    feat: enhance repository interfaces for patient and consultation management
    - Created PacienteRepository for managing patient records, with a method to retrieve all active patients using pagination.
    - Developed ConsultaRepository for managing consultations, including methods to check for existing consultations based on the doctor and date/time.
    - Added a custom query method to the MedicoRepository to find available doctors based on a specified date and time (findDisponiveisByDataHora), enabling scheduling functionality.

--- src/main/java/org/FelipeBert/api/repository/MedicoRepository.java
+++ src/main/java/org/FelipeBert/api/repository/MedicoRepository.java
@@ -4,7 +4,16 @@ import org.FelipeBert.api.model.Medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("SELECT m FROM Medico m WHERE m.dataHora = :dataHora")
+    List<Medico> findDisponiveisByDataHora(LocalDateTime dataHora);
 }
