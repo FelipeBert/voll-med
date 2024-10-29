9cb09f42ea7da2ccb2ce647a9d1335f7d5bff849
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-29 20:11:24 -0300

    feat: Implement security configurations with JWT authentication
    - SecurityConfigurations: Sets up security configurations with a custom filter and stateless authentication. Allows public access to the login endpoint and secures all other endpoints.
    - SecurityFilter: A filter that intercepts each request to validate the JWT token and authenticate the user, ensuring resource security.
    - TokenService: Service responsible for generating and validating JWT tokens. Uses HMAC256 and sets a 2-hour expiration for each token.

---
+++ src/main/java/org/FelipeBert/api/infra/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package org.FelipeBert.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.FelipeBert.api.infra.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenService service;
+
+    private UsuarioRepository repository;
+
+    public SecurityFilter(UsuarioRepository repository, TokenService service) {
+        this.repository = repository;
+        this.service = service;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+
+        if(tokenJwt != null){
+            var subject = service.getSubject(tokenJwt);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
